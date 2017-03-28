f15a1bcd63823ebc28542475e0a79ca832a28adf
Jonathan Chen <jchen2019@pingry.org>
2017-03-28 13:50:22 -0400

    Wrote code for basic doubling.

---
+++ multiples.py
@@ -0,0 +1,9 @@
+
+# Ask for user input
+userNumber = raw_input("Tell me a number. ")
+
+# Convert to float
+userNumber = float(userNumber)
+
+# Do the doubling
+print("Double that is {}. ".format(userNumber * 2))
